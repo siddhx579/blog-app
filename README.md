bcf7c678cc0cff5211569dec91b62588f0c7f860
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-07-17 00:01:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# blog-app
+This is beginner's project of making a blog-app using Vue.js
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app">
+    <h1>Vue Blog App</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+.app {
+  padding: 20px;
+}
+</style>
